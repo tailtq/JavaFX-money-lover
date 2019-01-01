80964671c6b3f2b4561cd742cb753c19f7685b8f
Tai.Ltq <tai.ltq@terra-drone.co.jp>
2019-01-01 11:07:14 +0700

    transaction 2

--- src/main/java/com/app/controllers/Pages/TransactionController.java
+++ src/main/java/com/app/controllers/Pages/TransactionController.java
@@ -1,14 +1,31 @@
 package main.java.com.app.controllers.Pages;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.MapChangeListener;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import main.java.com.app.controllers.Contracts.LoaderInterface;
 
 import java.io.IOException;
 
 public class TransactionController implements LoaderInterface {
+
+    @FXML
+    Button leftTime, middleTime, rightTime;
+
+    @FXML
+    VBox transactionContent;
+
+    @FXML
+    Label inflow;
 
     @Override
     public VBox loadView() throws IOException {
@@ -18,7 +35,14 @@ public class TransactionController implements LoaderInterface {
     }
 
     @FXML
-    public void changeTime(Event event) {
-//        even
+    public void changeTime(Event e) {
+        Node button = (Node) e.getSource();
+//        if (button == leftTime) {
+//            middleTime.setText("Hello");
+//        } else if (button == rightTime) {
+//            middleTime.setText("Good morning");
+//        }
     }
+
+
 }
